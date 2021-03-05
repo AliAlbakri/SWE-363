1a12d1fb197339b0b1fa99d4a101d018cfa6d2c5
Ali Albakri <albakri.aliahmed@gmail.com>
2021-03-06 01:25:33 +0300

    adding review section is added

--- js/index.js
+++ js/index.js
@@ -33,3 +33,46 @@ function addQuantityToCart(){
 }
 
 //___________________________________________________
+
+// Form Functionalities
+
+
+let form = document.getElementById("form")
+let msg = document.getElementById("msgForEmptyForm")
+let customerName = document.getElementById("name")
+let feedback = document.getElementById("feedback")
+let formWrapper = document.getElementById("formWrapper")
+
+$("#addRevBtn").click(function () {
+	$("#form").removeClass("hide")
+    $("#form").animate({ "left": 0 }, "slow");
+});
+
+function validateForm(){
+	
+	if (feedback.value == ""){
+		msg.classList.remove("hide")
+	}
+	else{
+		if(customerName.value.length == 0){
+			customerName.value = "Customer"
+		}
+		form.submit()
+	}
+	
+
+}
+
+let counter = document.getElementById("counter")
+feedback.addEventListener('input', function(e) {
+    const target = e.target;
+    const currentLength = target.value.length;
+    counter.innerHTML = `${currentLength}/500`;
+});
+
+function removeErrorMessage(){
+	msg.classList.add("hide")
+	
+}
+
+
